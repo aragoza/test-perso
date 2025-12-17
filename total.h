a9f9bd7db758599c54528409a40c595a6051ce0c
aragoza <charlet.elliot@gmail.com>
2025-12-17 22:35:47 +0100

    all the prototype

--- total.h
+++ total.h
@@ -1,8 +1,13 @@
 #ifndef _TOTAL_H_
 #define _TOTAL_H_
 
+typedef int pid_t;
+
+int waitpid(pid_t pid, int *status, int options);
 int print_arg(int argc, char **argv);
-int wait_for_line(void);
-int tokenize_a_sentence(int number_arg, char *arguments[]);
+char *wait_for_line(void);
+char **tokenize_a_sentence(char *line, char *delim);
+void free_tokens(char **argv);
+void execute_command(char **args);
 
 #endif /** _TOTAL_H_ */
